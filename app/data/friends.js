0dadcd04da1305672f0078c62e29496be8d792b1
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-05 11:07:08 -0700

    Added data into friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+      name: "Adam Levine",
+      image: "Gif/adamLevine.gif",
+      scores: [
+        "5",
+        "1",
+        "4",
+        "4",
+        "5",
+        "1",
+        "2",
+        "5",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Bruno Mars",
+      image: "Gif/brunoMars.gif",
+      scores: [
+        "3",
+        "5",
+        "5",
+        "4",
+        "4",
+        "3",
+        "2",
+        "5",
+        "3",
+        "2"
+      ]
+    },
+    {
+      name: "Justin Bieber",
+      image: "Gif/justinBieber.gif",
+      scores: [
+        "5",
+        "2",
+        "3",
+        "5",
+        "4",
+        "1",
+        "3",
+        "2",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "Lady Gaga",
+      image: "Gif/ladyGaga.gif",
+      scores: [
+        "4",
+        "2",
+        "4",
+        "3",
+        "3",
+        "1",
+        "5",
+        "5",
+        "4",
+        "3"
+      ]
+    },
+    {
+      name: "Michael Jackson",
+      image: "Gif/micheaelJackson.gif",
+      scores: [
+        "4",
+        "5",
+        "4",
+        "2",
+        "3",
+        "2",
+        "4",
+        "5",
+        "2",
+        "4"
+      ]
+    },
+    {
+      name: "Taylor Swift",
+      image: "Gif/taylorSwift.gif",
+      scores: [
+        "3",
+        "3",
+        "2",
+        "4",
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "1"
+      ]
+    }
+  
+  ];
+  
+  // export the array
+  module.exports = friends;
