772dfa69f65c75347df0c6b9cad97153365f418e
Kanyarut Pornamnuay <benbaba2525@hotmail.com>
2020-07-07 23:01:07 -0700

    Add package.json

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 9990;
+var PORT = process.env.PORT || 8080;
 
 
 
-// Sets up the Express app to handle data parsing
-//app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
